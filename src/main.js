297ae37012aafdf0aa48e2b9727a4aef360735e5
Ben Wilcox <reccanti@gmail.com>
2017-02-26 02:20:15 -0500

    Configure webpack-dev-server
    Adds webpack-dev-server to the
    dev dependencies and updates
    all the necessary files to work
    with it

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
 import {Synth, Loop, Transport} from 'tone';
+import {data} from './testdata';
 
-const synth = new Synth().toMaster();
-synth.triggerAttackRelease("C4", "8n");
+// const synth = new Synth().toMaster();
+// synth.triggerAttackRelease("C4", "8n");
 
+let counter = 0;
 const loop = new Loop((time) => {
-    console.log(time);
-}, '4n');
+    if (counter < data.length) {
+        console.log(data[counter]);
+    }
+    counter++;
+}, '1m');
 
 loop.start('0m');
 
+// Transport.scheduleRepeat((time) => {
+//     console.log(Transport.ticks);
+//     if (Transport.ticks < data.length) {
+//         console.log(data[Transport.ticks]);
+//     }
+// }, "1m");
 Transport.start();
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
+const path = require('path');
 module.exports = {
     entry: './src/main.js',
     output: {
-        path: './dist',
+        path: path.resolve(__dirname, './dist'),
         filename: 'build.js'
     },
     module: {
