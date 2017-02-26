fd49f646c488b114f41b0a0a19c683205d940bcd
Ben Wilcox <reccanti@gmail.com>
2017-02-26 02:14:21 -0500

    Add testdata
    Adds testdata so that we can test
    the timing of Tone.js

---
+++ src/testdata.js
@@ -0,0 +1,78 @@
+
+export const data = [// [Steven]
+    "Life and death and love and birth",
+    "And peace and war on the planet Earth",
+    "Is there anything that's worth",
+    "More than peace and love on the  planet Earth",
+    "Whoahh come on and sing it with me",
+
+    // [Peridot]
+    "Sing?",
+
+    // [Steven]
+    "The words relate to the key",
+
+    // [Peridot]
+    "Key?",
+
+    // [Steven]
+    "If it's a pattern, if it's a pattern",
+    "Then just repeat after me",
+    "Life and death and love and birth",
+
+    // [Peridot]
+    "Life and death and love and birth",
+
+    // [Steven]
+    "Now you sing mi-fa-mi-mi-fa-mi-ti-la",
+
+    // [Steven and Peridot]
+    "And peace and war on the planet Earth",
+
+    // [Steven]
+    "Yes yes! That's it!",
+
+    // [Peridot]
+    "That's so easy",
+
+    // [Steven]
+    "Yeah but that's what fun about it",
+    "You should write something, you should write a song!",
+
+    // [Peridot]
+    "About what?",
+
+    // [Steven]
+    "Whatever you're thinking",
+
+    // [Peridot]
+    "I guess we're already here",
+    "I guess we already know",
+    "We've all got something to fear",
+    "We've all got nowhere to go",
+    "I think you're all insane!",
+    "But I guess I am too",
+    "Anybody would be",
+    "If they were stuck on Earth with you!",
+
+    // [Steven]
+    "Hehehehe yes!",
+    "Life and death and love and birth and",
+
+    // [Amethyst]
+    "Life and death and love and birth and",
+
+    // [Pearl]
+    "Life and death and love and birth and",
+    "Peace and war on the planet Earth",
+
+    // [Garnet]
+    "Is there anything that's worth more",
+
+    // [Steven]
+    "Is there anything that's worth more",
+
+    // [Peridot]
+    "Is there anything that's worth more",
+    "Than peace and love on the planet Earth"
+];
